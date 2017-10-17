a81c840e31e55a0e2b7a687e539c8d015ccdb38b
wasim <muhammad.wasim93@gmail.com>
2017-10-17 22:40:45 +0500

    edited conn file

--- conn.php
+++ conn.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","pos");
+$conn = mysqli_connect("localhost","livesites","wF2J%q06EnE05e","poswasim");
 if (!$conn) {
 	die("Connection failed: " . mysqli_connect_error());
 }
